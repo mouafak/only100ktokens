950649f83e1e31dba85b735297f876ad770e15e5
alali mouafak <al-ali-mouafak@outlook.fr>
2024-08-08 18:01:08 +0200

    correction design

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,19 @@ export default function Home() {
   return (
     <main className="flex-center w-full h-screen bg-custom-image ">
       <div className=" h-full w-full flex flex-col justify-between items-center">
-        <div className="h-full w-full bg-custom-blue-green  ">
+        <div className="h-full w-full bg-custom-blue-green max-h-24 ">
           <div className="flex justify-between items-center container h-full py-2">
-            <Image src={logo2} alt="TRUMP’S TRIUMPH" width={200} height={200} />
+            <Image
+              src={logo2}
+              alt="TRUMP’S TRIUMPH"
+              width={200}
+              height={200}
+              className=" w-32 md:w-48 "
+            />
             <Link href={"https://trumptriumphcoin.com/"}>
               <div className="flex gap-1 items-center">
-                <ArrowLeft className="w-5 h-5 stroke-white " />
-                <p className="text-white">Back to Home</p>
+                <ArrowLeft className="w-4 h-4 md:w-5 md:h-5 stroke-white " />
+                <p className="text-white text-sm md:text-base">Back to Home</p>
               </div>
             </Link>
           </div>
