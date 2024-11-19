4c6db7e4f0c6d2945102f37512d4fa7daac4490b
mouafak <al-ali-mouafak@outlook.fr>
2024-11-19 22:02:06 +0100

    update packages

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
     reactStrictMode: true,
     output: "standalone",
-    webpack: config => {
-        config.externals.push('pino-pretty', 'lokijs', 'encoding')
-        return config
-    }
+    // webpack: config => {
+    //     config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    //     return config
+    // }
 }
 export default nextConfig;
