7be6f46c7e9a3d1c9c8dbf4d9870c700d3c78ea5
mouafak <al-ali-mouafak@outlook.fr>
2025-08-04 16:25:40 +0200

    Hide progress bar and update excluded wallets

--- constant/excluded-wallets.ts
+++ constant/excluded-wallets.ts
@@ -4,6 +4,7 @@ export const EXCLUDED_WALLETS = [
     "9XJonoZy9qjjuAqU9YzQ4KEAcYfm1DMBnY9aZ1wKcobx",
     "BubftrxiJee1B6dQMj4Xp4T9rdrZuBzVtY5uYBr59Y6g",
     "SuGTd5urB6ffG3TEZdNdqZtjERJjDR8AZWyCF7Y7wp8",
+    "5trfggrEppGZzXKSHG6TgRd5ggXE6dP87oiFTW2sukBm",
     // "4V3Yx3aki6Kq2TkKM3L8Lj5hjAjaSsEhAvvTFeEuWxe9"
     "G6MDAqmTKaDGJrRUzrg9vQypkSESEbnutDGNixrqChB8"
 ];
