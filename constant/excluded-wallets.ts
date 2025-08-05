9eff4558efc3f674ff6426e2484e14d55ffedf72
mouafak <al-ali-mouafak@outlook.fr>
2025-08-05 23:17:23 +0200

    update excluded wallets_02

--- constant/excluded-wallets.ts
+++ constant/excluded-wallets.ts
@@ -5,7 +5,6 @@ export const EXCLUDED_WALLETS = [
     "BubftrxiJee1B6dQMj4Xp4T9rdrZuBzVtY5uYBr59Y6g",
     "SuGTd5urB6ffG3TEZdNdqZtjERJjDR8AZWyCF7Y7wp8",
     "5trfggrEppGZzXKSHG6TgRd5ggXE6dP87oiFTW2sukBm",
-    // "4V3Yx3aki6Kq2TkKM3L8Lj5hjAjaSsEhAvvTFeEuWxe9"
     "G6MDAqmTKaDGJrRUzrg9vQypkSESEbnutDGNixrqChB8",
     "SuGTd5urB6ffG3TEZdNdqZtjERJjDR8AZWyCF7Y7wp8"
 ];
